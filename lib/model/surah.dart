33e361b6794e917d83ed4c1b880951edb3014a0a
mfathirr <mulfathir632@gmail.com>
2024-05-21 11:19:04 +0700

    first commit

---
+++ lib/model/surah.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+import 'package:quran/model/ayah.dart';
+
+List<Surah> surahFromJson(String str) =>
+    List<Surah>.from(json.decode(str).map((x) => Surah.fromJson(x)));
+
+String surahToJson(List<Surah> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Surah {
+  int nomor;
+  String nama;
+  String namaLatin;
+  int jumlahAyat;
+  String tempatTurun;
+  String arti;
+  String deskripsi;
+  String audio;
+  List<Ayat>? ayat;
+
+  Surah(
+      {required this.nomor,
+      required this.nama,
+      required this.namaLatin,
+      required this.jumlahAyat,
+      required this.tempatTurun,
+      required this.arti,
+      required this.deskripsi,
+      required this.audio,
+      this.ayat});
+
+  // GET data from JSON to class Surah
+  factory Surah.fromJson(Map<String, dynamic> json) => Surah(
+      nomor: json['nomor'],
+      nama: json['nama'],
+      namaLatin: json['nama_latin'],
+      jumlahAyat: json['jumlah_ayat'],
+      tempatTurun: json['tempat_turun'],
+      arti: json['arti'],
+      deskripsi: json['deskripsi'],
+      audio: json['audio'],
+      ayat: json.containsKey('ayat')
+          ? List<Ayat>.from(json['ayat'].map((x) => Ayat.fromJson(x)))
+          : null);
+
+  Map<String, dynamic> toJson() => {
+        'nomor': nomor,
+        'nama': nama,
+        'namaLatin': namaLatin,
+        'jumlah_ayat': jumlahAyat,
+        'tempat_turun': tempatTurun,
+        'arti': arti,
+        'deskripsi': deskripsi,
+        'audio': audio,
+      };
+}
+
+enum TempatTurun { MADINAH, MAKKAH }
+
+final tempatTurunValues =
+    EnumValues({'madinah': TempatTurun.MADINAH, 'mekah': TempatTurun.MAKKAH});
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((key, value) => MapEntry(value, key));
+    return reverseMap;
+  }
+}
